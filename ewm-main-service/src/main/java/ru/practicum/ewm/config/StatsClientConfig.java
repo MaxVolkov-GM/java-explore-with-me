8b753dc54d7ab99b3caa87dacd76c8780c5aa6b5
MaxVolkov-GM <mrdarkvolkov@gmail.com>
2026-05-08 22:30:24 +0300

    prepare main service

---
+++ ewm-main-service/src/main/java/ru/practicum/ewm/config/StatsClientConfig.java
@@ -0,0 +1,14 @@
+package ru.practicum.ewm.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.practicum.stats.client.StatsClient;
+
+@Configuration
+public class StatsClientConfig {
+	@Bean
+	public StatsClient statsClient(@Value("${stats-server.url}") String statsServerUrl) {
+		return new StatsClient(statsServerUrl);
+	}
+}
